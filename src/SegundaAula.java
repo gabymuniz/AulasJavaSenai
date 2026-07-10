1d5ae1927c31f604d01e4f059a68f0e887eabbf0
gabymuniz <gabyi.amsmuniz@gmail.com>
2026-07-09 21:40:29 -0300

    Segunda aula. If, else e switch case.

--- src/SegundaAula.java
+++ src/SegundaAula.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
+
 public class SegundaAula {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int value = scanner.nextInt();
+
+        if(value < 10){
+            System.out.println("Menor do que 10");
+        }else{
+            System.out.println("Maior do que 10");
+        }
+
+        switch (value){
+            case 1:
+                System.out.println("Escolheu 1");
+                break;
+            case 2:
+                System.out.println("Escolheu 2");
+                break;
+            default:
+                System.out.println("Não escolheu uma opção válida!");
+        }
+    }
 }
